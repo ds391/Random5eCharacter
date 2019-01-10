cd542faa12e2a6a919c0efb12733cca73fcc9ce9
ds391 <31848881+ds391@users.noreply.github.com>
2019-01-10 12:37:02 +0000

    Create RandomCharacter.ps1

---
+++ RandomCharacter.ps1
@@ -0,0 +1,22 @@
+Function Alignment{
+	Get-Content '5e Alignment.txt' -TotalCount (Get-Random -Maximum (Get-Content '5e Alignment.txt').Count) | Select-Object -Last 1
+}
+Function Race{
+	Get-Content '5e Races.txt' -TotalCount (Get-Random -Maximum (Get-Content '5e Races.txt').Count) | Select-Object -Last 1
+}
+Function Classes{
+	Get-Content '5e Classes.txt' -TotalCount (Get-Random -Maximum (Get-Content '5e Classes.txt').Count) | Select-Object -Last 1
+}
+Function Background{
+	Get-Content '5e Backgrounds.txt' -TotalCount (Get-Random -Maximum (Get-Content '5e Backgrounds.txt').Count) | Select-Object -Last 1
+}
+Function Trinket{
+	Get-Content '5e Trinkets.txt' -TotalCount (Get-Random -Maximum (Get-Content '5e Trinkets.txt').Count) | Select-Object -Last 1
+}
+$Alignment = Alignment
+$Race = Race
+$Classes = Classes
+$Background = Background
+$Trinket = Trinket
+
+Write-Host 'I am a' $Alignment $Race $Classes $Background 'With' $Trinket
